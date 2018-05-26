a24e468f222af5d7c196b5a9387b7cd13dba523c
Sarah <sarah@Gregs-iMac.local>
2018-05-27 00:02:43 +1000

    commit before renaming, moved some functions

--- tools_for_digital_death_trip.rb
+++ tools_for_digital_death_trip.rb
@@ -2,7 +2,7 @@ require "csv"
 require "net/http"
 require "date"
 require "rbconfig"
-load 'tools_for_talking.rb'
+load 'tools_for_general_use.rb'
 
 def return_town_list(source_choice, input_path_name)
    result = false
@@ -36,7 +36,7 @@ def return_town_list(source_choice, input_path_name)
 end
 
 
-def return_town_list_from_existing_ptv_stop_files(input_path_name, gtfs_file_name='gtfs.zip', default_stop_file_name='stops.txt', stop_field_num=1)
+def return_town_list_from_existing_ptv_stop_files(input_path_name, default_stop_file_name='stops.txt', stop_field_num=1)
    result = false
    begin
       full_town_list = Array.new 
@@ -153,105 +153,6 @@ def unzip_ptv_gtfs_file(input_path_name, gtfs_file_name='gtfs.zip', path_numbers
 end
 
 
-def unzip_single_file(input_file_name, input_path_name=nil, overwrite=true, output_path_name=nil)
-   # unzips a single zip file
-   # most code borrowed from https://gist.github.com/robc/217400
-   # returns the output path if successful, or false if error encountered
-   # if output_path_name is left as nil then it will construct a default output location using zip file norms
-   result = false
-   begin
-      puts("2.1")
-      puts(input_file_name)
-      puts(input_path_name)
-      puts(overwrite)
-      if (input_path_name != nil) then
-         full_zip_file_name = File.join(input_path_name, input_file_name)
-      else
-         full_zip_file_name = input_file_name
-      end
-      puts("2.2")
-      puts(full_zip_file_name)
-      if (output_path_name == nil) then
-         if (input_path_name == nil) then
-            full_output_path_name = File.dirname(full_zip_file_name)
-         else   
-            full_output_path_name = File.join(input_path_name, File.basename(full_zip_file_name, '.zip'))
-         end
-      else
-         full_output_path_name = output_path_name
-      end
-      puts("here2")
-      puts(output_path_name)
-      result = unzip_file_with_7z_command(full_zip_file_name = full_zip_file_name, full_output_path_name = full_output_path_name, overwrite = overwrite)
-      
-      if (result == false) then
-         result = unzip_file_with_unzip_command(full_zip_file_name = full_zip_file_name, full_output_path_name = full_output_path_name, overwrite = overwrite)
-      end
-      
-      return(full_output_path_name)
-
-   rescue   
-      puts("Error encountered with unzipping #{input_file_name}") 
-      return(result)
-   end
-end
-
-
-def unzip_file_with_unzip_command(full_zip_file_name, full_output_path_name=nil, overwrite=true)
-   # tries unzipping with the 'unzip' command, returns true if successful, false if error encountered
-   result = false
-   begin
-      puts("unzip_file_with_unzip_command")
-      command_string = "unzip "
-      if (overwrite == true) then
-         command_string += "-o "
-      end
-      command_string += full_zip_file_name
-      if (full_output_path_name != nil) then
-         command_string += " -d " + full_output_path_name
-      end
-      puts(command_string)
-      system(command_string)
-      puts("Successfully unzipped #{full_zip_file_name} to #{output_path_name} using 'unzip' command")
-      result = true
-      return(result)
-   rescue
-      puts("Error encountered with unzipping #{full_zip_file_name} to #{full_output_path_name}using 'unzip' command")
-      return(result)
-   end
-end
-
-
-def unzip_file_with_7z_command(full_zip_file_name, full_output_path_name=nil, overwrite=true)
-   # tries unzipping with the 'unzip' command line, returns true if successful, false if error encountered
-   result = false
-   begin
-      puts("unzip_file_with_7z_command")
-      if (operating_system() == 'windows') then
-         command_string = "7z"
-      else
-         command_string = "7za"
-      end
-      command_string += " x " 
-      if (overwrite == true) then
-         command_string += "-aoa "
-      end
-      command_string += full_zip_file_name
-      if (full_output_path_name != nil) then
-         command_string += " -o" + full_output_path_name
-      end
-      puts(command_string)
-      system(command_string)
-      puts("Successfully unzipped #{full_zip_file_name} to #{full_output_path_name} using '7z' command")
-      result = true
-      return(result)
-   rescue
-      puts("Error encountered with unzipping #{full_zip_file_name} to #{full_output_path_name} using '7z' command")
-      return(result)
-   end
-end
-
-
 def return_town_list_from_vicmap(input_path_name, search_state='VIC', vicmap_csv_file_name='vic_and_border_locality_list.csv', town_field_num=5, state_field_num=6)
    result = []
    puts("Attempting to read VicMap town list from #{File.join(input_path_name, vicmap_csv_file_name)}")
@@ -277,25 +178,6 @@ def return_town_list_from_vicmap(input_path_name, search_state='VIC', vicmap_csv
 
 end
 
-def convert_phrase_string_for_url(input_string, input_divider = ' ', output_quotemark='%22', output_divider='%20')
-   # changes a phrase string with spaces, to string suitable for use in a URL
-   # treats as a single phrase, not as separate words
-   # (i.e. puts a URL-friendly quote symbol at the start and end, and URL-friendly dividers in the middle)
-   # https://www.w3schools.com/tags/ref_urlencode.asp
-   input_words = input_string.split(input_divider)
-   if (input_words.size == 1) then
-      return(input_string)
-   end
-   
-   output_string = output_quotemark
-   for word in input_words.each do
-      output_string += (word + output_divider)
-   end
-
-   output_string = output_string[0..output_divider.size] + output_quotemark
-   return(output_string)
-
-end
 
 
 # POTENTIAL FUNCTION: READING LIST OF EXISTING FILES

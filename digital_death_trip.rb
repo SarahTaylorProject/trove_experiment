e5f9d40674e7716c252bfbc29f949ebf96803d6a
Sarah <sarah@Gregs-iMac.local>
2018-07-29 14:41:22 +1000

    trying change to just read_trove_headlines

--- digital_death_trip.rb
+++ digital_death_trip.rb
@@ -80,57 +80,82 @@ else
 end
 
 # nb. need function here to gather coordinates for town choices not made through town_dictionary
+# START PASTE
+
+ if (continue == true) then
+    say_something("Ok. I will now see if I can find any newspaper references to a #{search_word} in #{search_town}")
+    output_file_name = File.join(default_output_path, "trove_result_#{search_town}_#{search_word}.csv".gsub(/\s/,"_"))
+    trove_api_results = fetch_trove_results(search_town, search_word, my_trove_key)
+    puts("\nWriting results to file now...")
+    result_count = write_trove_results(trove_api_results, output_file_name, search_word, search_town)
+    puts(result_count)
+
+    result_count = preview_trove_results(output_file_name)
+    if (result_count == 0) then
+       continue = false
+       say_something("\nSorry, no tragedy results found for #{search_town}")
+    end
+ end
+
+
+default_article_numbers = Array(1..5)
+random_article_range = Array(1..20)
+# search_town = 'Elmore'
+output_file_name = File.join(default_output_path, "trove_result_#{search_town}_#{search_word}.csv".gsub(/\s/,"_"))
 
 if (continue == true) then
-   say_something("Ok. I will now see if I can find any newspaper references to a #{search_word} in #{search_town}")
-   output_file_name = File.join(default_output_path, "trove_result_#{search_town}_#{search_word}.csv".gsub(/\s/,"_"))
-   trove_api_results = fetch_trove_results(search_town, search_word, my_trove_key)
-   puts("\nWriting results to file now...")
-   result_count = write_trove_results(trove_api_results, output_file_name, search_word, search_town)
-   puts(result_count)
-
    result_count = preview_trove_results(output_file_name)
-   if (result_count == 0) then
+   say_something("\nI found some results.\nWould you like me to read a few headlines, to get a sense of the tragedies in #{search_town}?", also_print = true, speed = default_speed)
+   user_input = get_user_input(prompt_text = "Enter 'n' if not interested, \nEnter 'exit' to cancel entirely, \nEnter 'all' to hear all the headlines, \nEnter any other key to hear a few sample headlines...")
+   if (user_input.upcase == 'EXIT') then
       continue = false
-      say_something("\nSorry, no tragedy results found for #{search_town}")
-   else
-      say_something("\nI found some results.\nWould you like me to read the headlines?", also_print = true, speed = default_speed)
-      user_input = get_user_input(prompt_text = "Enter 'n' if not interested, \nEnter 'exit' to cancel entirely, \nEnter any other key to hear headlines...")
-      if (user_input.upcase == 'EXIT') then
-         continue = false
-      elsif (user_input.upcase != 'N') then
-         read_trove_headlines(input_trove_file = output_file_name, speed = default_speed)
-      end
+   elsif (user_input.upcase == 'ALL') then
+      # just reads the default first 5
+      read_trove_headlines_sample(input_trove_file = output_file_name, speed = default_speed, article_numbers = default_article_numbers)
+   elsif (user_input.upcase != 'N') then
+      # reads random sample of 5
+      random_article_numbers = Array.new(5) { rand(1..20) }
+      read_trove_headlines_sample(input_trove_file = output_file_name, speed = default_speed, article_numbers = random_article_numbers)
    end
 end
 
-default_article_numbers = [1, 2, 3, 4]
 while (continue == true) do   
    clear_screen()
    result_count = preview_trove_results(output_file_name)
-   say_something("\nWould you like me to read any article content?", also_print = true, speed = default_speed)  
-   user_input = get_user_input(prompt_text = "\Please enter article numbers separated by space or comma. \nEnter 'n' or exit' to cancel.\nI will default to #{default_article_numbers}")
-         
+   
+   say_something("\nShall I pick a random tragedy from this place? Or let me know if you would like to pick from some specific articles", also_print = true, speed = default_speed)  
+   user_input = get_user_input(prompt_text = "\nI will default to a random selection. \nPlease enter 'pick' if you would like to pick. \nEnter 'n' or exit' to cancel.")
    if (user_input.upcase == 'N' or user_input.upcase == 'EXIT') then
       continue = false
-   else
-      article_numbers = return_int_array_from_string(user_input, divider = ",")
-      if (article_numbers == false) then
-        article_numbers = return_int_array_from_string(user_input, divider = " ")
-      end           
-
-      if (article_numbers == false) then
+   elsif (user_input.upcase == 'PICK') then 
+      say_something("\nWhich articles are you interested in?", also_print = true, speed = default_speed)  
+      user_input = get_user_input(prompt_text = "\Please enter article numbers separated by space or comma. \nEnter 'n' or exit' to cancel.\nI will default to #{default_article_numbers}")   
+      if (user_input.upcase == 'N' or user_input.upcase == 'EXIT') then
          continue = false
-      elsif (article_numbers.size == 0) then
-         article_numbers = default_article_numbers
+      else
+         article_numbers = return_int_array_from_string(user_input, divider = ",")
+         if (article_numbers == false) then
+            article_numbers = return_int_array_from_string(user_input, divider = " ")
+         end           
+         if (article_numbers == false) then
+            continue = false
+         elsif (article_numbers.size == 0) then
+            article_numbers = default_article_numbers
+         end
       end
-   end
+      if (continue == true) then
+         say_something("Ok. I will read articles #{article_numbers}", also_print = true, speed = default_speed)
+         read_trove_results_by_array(input_trove_file = output_file_name, article_numbers = article_numbers, speed = default_speed)
+      end
 
-   if (continue == true) then
-      say_something("Ok. I will read articles #{article_numbers}", also_print = true, speed = default_speed)
-      read_trove_results_by_array(input_trove_file = output_file_name, article_numbers = article_numbers, speed = default_speed)
-   end
+   else
+      random_article_number = random_article_range.sample
+      say_something("Ok. Here is my random tragedy from #{search_town}.", also_print = true, speed = default_speed)
+      read_trove_results_by_array(input_trove_file = output_file_name, article_numbers = [random_article_number], speed = default_speed)
+   end               
 end
+
+# END PASTE
 
 say_something("\nOk. Before I go, would you like me to update your map files?", also_print = true, speed = default_speed)
 user_input = get_user_input(prompt_text = "Enter 'n' if not interested\nEnter any other key to update map files...")

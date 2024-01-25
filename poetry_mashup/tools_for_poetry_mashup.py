4d26e8e80f10767d33ff3d11456a9a0c1e36f5d8
SarahTaylorProject <sarah.taylor@rmit.edu.au>
2024-01-25 14:42:25 +1100

    successfully moved tools_for_general_use into a shared location

--- poetry_mashup/tools_for_poetry_mashup.py
+++ poetry_mashup/tools_for_poetry_mashup.py
@@ -1,10 +1,9 @@
-import subprocess
-import os
 import traceback
-import sys
 import requests
 import json
+import sys
 import random
+from random import sample
 
 import string
 from nltk.corpus import stopwords
@@ -12,6 +11,9 @@ from nltk.corpus import words
 
 from nltk.tokenize import word_tokenize
 import nltk
+
+sys.path.insert(0, '..')
+from tools_for_general_use import *
 
 # comment these out if using repeatedly
 # nltk.download('stopwords')
@@ -21,112 +23,6 @@ import nltk
 stop_words = set(stopwords.words('english'))
 corpus_words = set(w.lower() for w in nltk.corpus.brown.words())
 corpus_words_sorted = sorted(corpus_words)
-
-from random import sample
-
-
-def return_operating_system():
-  """
-  This function returns a string of the operating system currently in use
-  """
-  result = ""
-  from sys import platform
-  if platform == "linux" or platform == "linux2":
-    result = "linux"
-  elif platform == "darwin":
-    result = "mac"
-  elif platform == "win32":
-    result = "win32"
-  return(result)
-
-
-def test_say_something(text="test", speed=120, espeak_executable_path='C:\Elevate\eSpeak NG\espeak-ng.exe'):
-  """
-  # Test say_something options
-  Returns True if no error
-  Returns False if error
-  """
-  result = False
-  try:
-    os_result = return_operating_system()
-    command_text = None
-    if os_result == "mac":
-      command_text = 'say -r ' + ' "' + text + '"'
-    elif (os.path.isfile(espeak_executable_path)):
-      command_text = '"{0}" -s {1}'.format(espeak_executable_path, speed)
-      command_text += ' "' + remove_nuisance_characters_from_string(text) + '"'
-    elif os_result == "win32":
-      command_text = 'espeak -s' + str(speed) + ' "' + text + '"'
-    elif os_result == "linux":
-      command_text = "echo '" + text + "'|espeak -s " + str(speed)
-
-    if (command_text != None):
-      subprocess.check_output(command_text)
-      result = True
-
-    return(result)
-  
-  except:
-    return(result)
-
-
-
-def say_something(text, also_print=True, try_say=True, speed=120, espeak_executable_path='C:\Elevate\eSpeak NG\espeak-ng.exe'):
-  """
-  # Adapted from original Ruby script, August 2017
-  # This function says text aloud through the command line for some operating systems
-  # It checks for operating system and uses appropriate say-aloud command line
-  # Works for linux and mac, and for Windows if the 'espeak' package is installed.
-  # If try_say is passed in as False, it will not bother with trying to say stuff
-  # If also_print is True, then the text is sent to puts as well
-  """
-
-  if (also_print == True):
-    print(text)
-  if (try_say == False):
-    return()
-
-  os_result = None
-  os_result = return_operating_system()
-  command_text = None
-
-  if os_result == "mac":
-    command_text = 'say -r ' + str(speed) + ' "' + text + '"'
-  elif (espeak_executable_path != None and os.path.isfile(espeak_executable_path)):
-    print("here")
-    command_text = '"{0}" -s {1}'.format(espeak_executable_path, speed)
-    command_text += ' "' + remove_nuisance_characters_from_string(text) + '"'
-  elif os_result == "win32":
-    command_text = 'espeak -s' + str(speed) + ' "' + text + '"'
-  elif os_result == "linux":
-    command_text = "echo '" + text + "'|espeak -s " + str(speed)
-
-  if (command_text != None):
-    subprocess.call(command_text, shell=True)
-  else:
-    return()
-    ##print("\t(say_something does not yet support this operating system)")
-
-
-def get_user_input(prompt_text = "\nPlease enter value"):
-  # This method just gets direct input from the user with a prompt
-  # Returns the user input
-  # Tried two options for Python input: rawinput or input (one is in each function below)
-  # If the 'input' function returns False, it will try the 'rawinput' function
-  # Returns input_text for user input, which will be False if both functions failed
-  result = False
-  try:
-    if (sys.version_info > (3, 0)):
-      input_text = input(prompt_text)
-    else:
-      input_text = raw_input(prompt_text)
-
-    return(input_text)
-
-  except:
-    traceback.print_exc()
-    return(result)
-
 
 def return_random_poetry(author_list=["Browning", "Byron", "Dickinson", "Po", "Shakespeare", "Shelley", "Tennyson", "Wilde"], min_length=8, max_tries=10, full_metadata=True, decode_result=True):
   """
@@ -411,53 +307,8 @@ def read_text_file_to_array(input_file_name, max_lines = None):
     traceback.print_exc()
     return(result)
 
-# def return_array_of_strings_also_split_by_character(input_array, split_character="."):
-#   result = input_array
-#   try:
-#     if (split_character == None):
-#       return(input_array)
-#     output_array = []
-#     for input_line in input_array:
-#       if ((split_character in input_line) and (input_line.endswith(split_character) == False)):
-#         input_line_split = input_line.split(split_character)
-#         output_array.extend(input_line_split)
-#       else:
-#         output_array.append(input_line)
-#     return(output_array)
-#   except:
-#     traceback.print_exc()
-#     return(result)
 
 
-
-# def remove_item_from_list(input_list, remove_item=""):
-#   try:
-#     for item in input_list:
-#       if item == remove_item:
-#         input_list.remove(item)
-#     return(input_list)
-#   except:
-#     traceback.print_exc()
-#     return(None)
-
-
-def remove_nuisance_characters_from_string(input_string):
-  """
-  This function is a workaround for the bug that Python 3 decoding doesn't work.
-  It removes some typical UTC characters that may be returned from API calls and problematise use of a string in an email header.
-  """
-  try:
-    input_string = input_string.replace('\n', ' ')
-    input_string = input_string.replace(u"\u201c", "'")
-    input_string = input_string.replace(u"\u201d", "'")
-    input_string = input_string.replace(u"\u2014", "")
-    input_string = input_string.replace(u"\u2018", "")
-    input_string = input_string.replace(u"\u2019", "")
-    input_string = input_string.replace('  ', ' ')
-    return(input_string)
-  except:
-    traceback.print_exc()
-    return(input_string)
 
 
 ### NLTK FUNCTIONS
---
+++ tools_for_general_use.py
@@ -0,0 +1,135 @@
+import os
+import sys
+import traceback
+from pathlib import Path
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    return()
+
+def return_script_directory():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    return(script_directory)
+
+def return_parent_directory(input_directory):
+    parent_directory = Path(input_directory).parent.absolute()
+    return(parent_directory)
+
+def return_operating_system():
+  """
+  Returns a string of the operating system currently in use
+  """
+  result = ""
+  from sys import platform
+  if platform == "linux" or platform == "linux2":
+    result = "linux"
+  elif platform == "darwin":
+    result = "mac"
+  elif platform == "win32":
+    result = "win32"
+  return(result)
+
+
+def test_say_something(text="test", speed=120, espeak_executable_path='C:\Elevate\eSpeak NG\espeak-ng.exe'):
+  """
+  Test of say_something: useful to include a call to this before using say_something repeatedly
+  Returns True if no error
+  Returns False if error
+  """
+  result = False
+  try:
+    os_result = return_operating_system()
+    command_text = None
+    if os_result == "mac":
+      command_text = 'say -r ' + ' "' + text + '"'
+    elif (os.path.isfile(espeak_executable_path)):
+      command_text = '"{0}" -s {1}'.format(espeak_executable_path, speed)
+      command_text += ' "' + remove_nuisance_characters_from_string(text) + '"'
+    elif os_result == "win32":
+      command_text = 'espeak -s' + str(speed) + ' "' + text + '"'
+    elif os_result == "linux":
+      command_text = "echo '" + text + "'|espeak -s " + str(speed)
+
+    if (command_text != None):
+      subprocess.check_output(command_text)
+      result = True
+
+    return(result)
+  
+  except:
+    return(result)
+
+
+
+def say_something(text, also_print=True, try_say=True, speed=120, espeak_executable_path='C:\Elevate\eSpeak NG\espeak-ng.exe'):
+  """
+  Says text aloud through the command line for some operating systems
+  Checks for operating system and uses appropriate say-aloud command line
+  Works for linux and mac, and for Windows if the 'espeak' package is installed.
+  If try_say is passed in as False, it will not bother with trying to say stuff
+  If also_print is True, then the text is sent to puts as well
+  """
+
+  if (also_print == True):
+    print(text)
+  if (try_say == False):
+    return()
+
+  os_result = None
+  os_result = return_operating_system()
+  command_text = None
+
+  if os_result == "mac":
+    command_text = 'say -r ' + str(speed) + ' "' + text + '"'
+  elif (espeak_executable_path != None and os.path.isfile(espeak_executable_path)):
+    print("here")
+    command_text = '"{0}" -s {1}'.format(espeak_executable_path, speed)
+    command_text += ' "' + remove_nuisance_characters_from_string(text) + '"'
+  elif os_result == "win32":
+    command_text = 'espeak -s' + str(speed) + ' "' + text + '"'
+  elif os_result == "linux":
+    command_text = "echo '" + text + "'|espeak -s " + str(speed)
+
+  if (command_text != None):
+    subprocess.call(command_text, shell=True)
+  else:
+    return()
+
+
+def get_user_input(prompt_text = "\nPlease enter value"):
+  # This method just gets direct input from the user with a prompt
+  # Returns the user input
+  # Tried two options for Python input: rawinput or input (one is in each function below)
+  # If the 'input' function returns False, it will try the 'rawinput' function
+  # Returns input_text for user input, which will be False if both functions failed
+  result = False
+  try:
+    if (sys.version_info > (3, 0)):
+      input_text = input(prompt_text)
+    else:
+      input_text = raw_input(prompt_text)
+
+    return(input_text)
+
+  except:
+    traceback.print_exc()
+    return(result)
+
+
+def remove_nuisance_characters_from_string(input_string):
+  """
+  This function is a workaround for the bug that Python 3 decoding doesn't work.
+  It removes some typical UTC characters that may be returned from API calls and problematise use of a string in an email header.
+  """
+  try:
+    input_string = input_string.replace('\n', ' ')
+    input_string = input_string.replace(u"\u201c", "'")
+    input_string = input_string.replace(u"\u201d", "'")
+    input_string = input_string.replace(u"\u2014", "")
+    input_string = input_string.replace(u"\u2018", "")
+    input_string = input_string.replace(u"\u2019", "")
+    input_string = input_string.replace('  ', ' ')
+    return(input_string)
+  except:
+    traceback.print_exc()
+    return(input_string)

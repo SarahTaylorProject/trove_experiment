177d3f800ffaa8a21d859c28622fe958f4c0bc88
SarahTaylorProject <sarah.taylor@rmit.edu.au>
2024-01-25 12:51:46 +1100

    updated read_random_bible_verse to use the test of say_something

--- poetry_mashup/read_random_bible_verse.py
+++ poetry_mashup/read_random_bible_verse.py
@@ -4,6 +4,9 @@ import os
 import sys
 import requests
 
+try_say = test_say_something()
+print(try_say)
+
 default_speed = 170
 default_line_count = 1
 
@@ -13,7 +16,7 @@ book_list = return_bible_book_list()
 
 # 2. start talking to user
 greeting_string = "\nHello. I will choose a Bible verse for you.\nPlease wait while I ask the internet...\n"
-say_something(text=greeting_string, speed=default_speed)
+say_something(text=greeting_string, try_say=try_say, speed=default_speed)
 
 # 3. choose random bible verse, with maximum allowable tries
 random_bible_quotes = []
@@ -26,12 +29,12 @@ if (current_quote != False):
 # 4. 
 for quote in random_bible_quotes:
   #print(quote)
-  say_something(text=quote[0], speed=default_speed)
-  say_something(text=quote[1], speed=default_speed)
+  say_something(text=quote[0], try_say=try_say, speed=default_speed)
+  say_something(text=quote[1], try_say=try_say, speed=default_speed)
 
 if (len(random_bible_quotes) > 0):
   greeting_string = "\nThank you for listening.\nI trust you know what this means for you.\n"
-  say_something(text=greeting_string, speed=default_speed)
+  say_something(text=greeting_string, try_say=try_say, speed=default_speed)
 else:
   greeting_string = "\nSorry. The Bible did not answer for you today.\nPlease be abetter person and then try again.\n"
-  say_something(text=greeting_string, speed=default_speed)
+  say_something(text=greeting_string, try_say=try_say, speed=default_speed)

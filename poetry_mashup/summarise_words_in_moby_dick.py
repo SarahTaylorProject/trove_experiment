1fcea5c3debcb6bb0ed89a36e6e320e96136553c
SarahTaylorProject <sarah.taylor@rmit.edu.au>
2024-02-21 01:14:13 +1100

    added word tag summary: verb, adjective, etc.

--- poetry_mashup/summarise_words_in_moby_dick.py
+++ poetry_mashup/summarise_words_in_moby_dick.py
@@ -7,7 +7,7 @@ from nltk.corpus import stopwords
 
 stop_words = set(stopwords.words('english'))
 tokenizer = RegexpTokenizer(r'\w+')
-freq_display_count = 50
+freq_limit = 50
 
 corpus_file_id_list = [file_id for file_id in nltk.corpus.gutenberg.fileids()]
 corpus_file_id = 'melville-moby_dick.txt'
@@ -20,24 +20,37 @@ print(f"\nWord count (including stop words): {len(corpus_words_all)}")
 
 freq = nltk.FreqDist(corpus_words_all)
 most_common = freq.most_common(30)
-print(f"Most common {freq_display_count} words (including stop words):")
+print(f"Most common {freq_limit} words (including stop words):")
 print(most_common)
 
 corpus_words = [w.lower() for w in corpus_words_all if w.lower() not in stop_words]
 print(f"\nWord count (excluding stop words): {len(corpus_words)}")
 
 freq = nltk.FreqDist(corpus_words)
-most_common = freq.most_common(freq_display_count)
-print(f"Most common {freq_display_count} words (excluding stop words):")
+most_common = freq.most_common(freq_limit)
+print(f"Most common {freq_limit} words (excluding stop words):")
 print(most_common)
 
 common_word_list = []
 for item in most_common:
     common_word_list.append(item[0])
 
-test_words = ['man', 'woman', 'child']
+test_words = ['man', 'woman', 'child', 'whale']
 print(f"\nWord counts for: {test_words}")
 corpus_test_words = [w.lower() for w in corpus_words if w.lower() in test_words]
 freq = nltk.FreqDist(corpus_test_words)
-most_common = freq.most_common(freq_display_count)
+most_common = freq.most_common(freq_limit)
 print(most_common)
+
+#nltk.download('averaged_perceptron_tagger')
+freq_limit = 20
+corpus_words_tagged = nltk.pos_tag(corpus_words)
+tag_list = ['NN', 'NNS', 'JJ', 'VB', 'VBG']
+for tag in tag_list:
+    corpus_words_tag = [w[0] for w in corpus_words_tagged if w[1] == tag]
+    freq = nltk.FreqDist(corpus_words_tag)
+    most_common = freq.most_common(freq_limit)
+    print(f"\nMost common {freq_limit} words of type {tag}:")
+    print(most_common)
+
+print(f"Text: {corpus_file_id}")
